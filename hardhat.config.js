202c9fa873370b8f1c416ae606a670a7efcf82a1
GDYG <gdyg01@gmail.com>
2025-06-07 15:46:53 +0800

    update: switch localhost network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
-require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -14,13 +14,17 @@ module.exports = {
   },
   networks: {
     hardhat: {
-      chainId: 1337,
+      chainId: 31337,
     },
-    sepolia: {
-      url: process.env.SEPOLIA_RPC_URL,
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
-      chainId: 11155111,
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337
     },
+    // sepolia: {
+    //   url: process.env.SEPOLIA_RPC_URL,
+    //   accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    //   chainId: 11155111,
+    // },
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
